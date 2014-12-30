553da2e6c984746840b01e0a93850f828db89307
euromark <euromark@web.de>
2014-12-30 12:51:51 +0100

    MaintenanceComponent

---
+++ Controller/Component/MaintenanceComponent.php
@@ -0,0 +1,35 @@
+<?php
+
+App::uses('Component', 'Controller');
+App::uses('FlashComponent', 'Tools.Controller/Component');
+App::uses('Configure', 'Core');
+
+/**
+ *
+ * @author Mark Scherer
+ * @license MIT
+ */
+class MaintenanceComponent extends Component {
+
+	//public $components = array('Tools.Flash');
+
+	protected $_defaultConfig = array(
+		'check' => true,
+	);
+
+	/**
+	 * Main interaction.
+	 *
+	 * @param Controller $controller
+	 * @return void
+	 */
+	public function initialize(Controller $Controller) {
+		// maintenance mode?
+		if ($overwrite = Configure::read('Maintenance.overwrite')) {
+			// if this is reachable, the whitelisting is enabled and active
+			$message = __d('setup', 'Maintenance mode active - your IP %s is in the whitelist.', $overwrite);
+			FlashComponent::transientMessage($message, 'warning');
+		}
+	}
+
+}
--- Controller/Component/SetupComponent.php
+++ Controller/Component/SetupComponent.php
@@ -12,7 +12,6 @@ App::uses('Component', 'Controller');
 App::uses('Folder', 'Utility');
 App::uses('ChmodLib', 'Tools.Utility');
 App::uses('SetupLib', 'Setup.Lib');
-App::uses('CommonComponent', 'Tools.Controller/Component');
 
 /**
  * Attach this to your AppController to power up debugging:
@@ -62,13 +61,6 @@ class SetupComponent extends Component {
 		// create missing tmp folders
 		if (Configure::read('debug') > 0) {
 			$this->setupDirs();
-		}
-
-		// maintenance mode?
-		if ($overwrite = Configure::read('Maintenance.overwrite')) {
-			// if this is reachable, the whitelisting is enabled and active
-			$message = __d('setup', 'Maintenance mode active - your IP %s is in the whitelist.', $overwrite);
-			CommonComponent::transientFlashMessage($message, 'warning');
 		}
 
 		// The following is only allowed with proper clearance
