c6fdcb13bbe94ef28cd226f958a38274227b1144
mscherer <mark.scherer@spryker.com>
2020-04-18 22:35:48 +0200

    Update bake templates.

--- templates/Bake/Plugin/src/Controller/AppController.php.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace <%= $namespace %>\Controller;
-
-use <%= $namespace %>\Controller\AppController as BaseController;
-
-class AppController extends BaseController
-{
-}
--- templates/Bake/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
-
-Router::plugin('<%= $plugin %>', function (RouteBuilder $routes) {
-    $routes->fallbacks();
-});
