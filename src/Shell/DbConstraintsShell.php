2ef6d22a3de6cdf97637c7241bdb55526116edb7
mscherer <mark.scherer@spryker.com>
2022-11-30 02:43:05 +0100

    Constraints shell

--- src/Shell/DbConstraintsShell.php
+++ src/Shell/DbConstraintsShell.php
@@ -31,9 +31,52 @@ class DbConstraintsShell extends Shell {
 		$models = $this->_getModels($modelName, $plugin);
 
 		$this->out('Checking ' . count($models) . ' models that need updating:', 1, static::VERBOSE);
+		$tables = [];
 		foreach ($models as $model) {
 			try {
-				$this->checkModel($model);
+				$tables += $this->checkModel($model);
+			} catch (CakeException $e) {
+				$this->err('Skipping due to errors: ' . $e->getMessage());
+
+				continue;
+			}
+		}
+
+		if ($tables && $this->param('verbose')) {
+			$this->out('');
+
+			$result = [];
+			foreach ($tables as $table => $fields) {
+				$result[] = '$this->table(\'' . $table . '\')';
+
+				foreach ($fields as $field => $relation) {
+					$result[] = "\t" . '->addForeignKey(\'' . $field . '\', \'' . $relation['table'] . '\', [\'' . $relation['field'] . '\'], [\'delete\' => \'SET_NULL\'])';
+				}
+
+				$result[] = "\t" . '->update();';
+			}
+
+			$this->out($result);
+		}
+
+		$this->out('');
+		$this->out('Done :) Possible nullable foreign key constraints checks executed.');
+	}
+
+	/**
+	 * @param string|null $modelName
+	 *
+	 * @return void
+	 */
+	public function fix($modelName = null) {
+		$plugin = $this->param('plugin');
+		$models = $this->_getModels($modelName, $plugin);
+
+		$this->out('Checking ' . count($models) . ' models that need fixing:', 1, static::VERBOSE);
+
+		foreach ($models as $model) {
+			try {
+				$this->fixModel($model);
 			} catch (CakeException $e) {
 				$this->err('Skipping due to errors: ' . $e->getMessage());
 
@@ -42,18 +85,18 @@ class DbConstraintsShell extends Shell {
 		}
 
 		$this->out('');
-		$this->out('Done :) Possible nullable foreign key constraints checks executed.');
+		$this->out('Done :) Possible nullable foreign key constraints fixes executed.');
 	}
 
 	/**
 	 * @param \Cake\ORM\Table $model
 	 *
-	 * @return void
+	 * @return array<string, array<string, mixed>>
 	 */
-	protected function checkModel(Table $model) {
+	protected function checkModel(Table $model): array {
 		$table = $model->getTable();
 		if (!$table) {
-			return;
+			return [];
 		}
 
 		$this->out('### ' . $table, 1, static::VERBOSE);
@@ -63,9 +106,10 @@ class DbConstraintsShell extends Shell {
 		$associations = $model->associations();
 		$relationKeys = $associations->keys();
 		if (!$relationKeys) {
-			return;
+			return [];
 		}
 
+		$fields = [];
 		foreach ($relationKeys as $relationKey) {
 			$relation = $associations->get($relationKey);
 			if ($relation->type() !== $relation::MANY_TO_ONE) {
@@ -110,6 +154,100 @@ class DbConstraintsShell extends Shell {
 			}
 
 			$this->warn('- Foreign key constraint missing: ' . $relation->getForeignKey());
+
+			$fields[$model->getTable()][$relation->getForeignKey()] = [
+				'table' => $relation->getTable(),
+				'field' => $relation->getPrimaryKey(),
+			];
+		}
+
+		return $fields;
+	}
+
+	/**
+	 * @param \Cake\ORM\Table $model
+	 *
+	 * @return void
+	 */
+	protected function fixModel(Table $model): void {
+		$table = $model->getTable();
+		if (!$table) {
+			return;
+		}
+
+		$this->out('### ' . $table, 1, static::VERBOSE);
+
+		$schema = $model->getSchema();
+
+		$associations = $model->associations();
+		$relationKeys = $associations->keys();
+		if (!$relationKeys) {
+			return;
+		}
+
+		$fields = [];
+
+		foreach ($relationKeys as $relationKey) {
+			$relation = $associations->get($relationKey);
+			if ($relation->type() !== $relation::MANY_TO_ONE) {
+				continue;
+			}
+
+			$this->out('Checking: ' . $model->getAlias() . '.' . $relation->getForeignKey() . ' => ' . $relation->getName() . '.' . $relation->getBindingKey());
+			$field = $schema->getColumn($relation->getForeignKey());
+			if (!$field) {
+				$this->warn(' - Cannot find column definition for `' . $relation->getForeignKey() . '`');
+			}
+			if ($field && ($field['null'] !== true || $field['default'] !== null)) {
+				continue;
+			}
+			// We only care about AIIDs for now
+			if ($field && $field['type'] !== 'integer') {
+				continue;
+			}
+
+			$ok = false;
+			$constraints = $schema->constraints();
+			foreach ($constraints as $constraint) {
+				$constraintDetails = $schema->getConstraint($constraint);
+				if ($constraintDetails['type'] !== 'foreign') {
+					continue;
+				}
+				if ($constraintDetails['columns'] !== [$relation->getForeignKey()]) {
+					continue;
+				}
+
+				$ok = true;
+
+				if (!empty($constraintDetails['delete']) && $constraintDetails['delete'] === 'setNull') {
+					continue;
+				}
+
+				//$this->warn('- Possibly missing a [\'delete\' => \'SET_NULL\'] constraint.');
+			}
+
+			if ($ok) {
+				continue;
+			}
+
+			$records = $model->find()
+				->contain([$relation->getName()])
+				->select([
+					$model->getAlias() . '.' . $model->getPrimaryKey(),
+					$model->getAlias() . '.' . $relation->getForeignKey(),
+					$relation->getName() . '.' . $relation->getPrimaryKey(),
+				])
+				->where([$relation->getForeignKey() . ' IS NOT' => null])
+				->all()
+				->toArray();
+			$property = $relation->getProperty();
+			foreach ($records as $record) {
+				if ($record->$property) {
+					continue;
+				}
+
+				$this->err('Invalid non-null foreign key for ID ' . $record->id);
+			}
 		}
 	}
 
@@ -135,6 +273,10 @@ class DbConstraintsShell extends Shell {
 			->setDescription('A Shell to check database integrity issues regarding nullable foreign key columns.')
 			->addSubcommand('check', [
 				'help' => 'Correct nullable foreign key columns by adding missing on delete constraints.',
+				'parser' => $subcommandParser,
+			])
+			->addSubcommand('fix', [
+				'help' => 'Correct nullable foreign key columns by setting invalid ones back to NULL.',
 				'parser' => $subcommandParser,
 			]);
 	}
