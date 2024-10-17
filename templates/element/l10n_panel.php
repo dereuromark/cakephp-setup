27fb433d75ffbdd21d38e379a5b841bed4cbef54
mscherer <dereuromark@web.de>
2024-10-17 03:26:44 +0200

    Add more info into L10n panel

--- templates/element/l10n_panel.php
+++ templates/element/l10n_panel.php
@@ -23,4 +23,46 @@ if (!isset($values)) {
 		<?php } ?>
 	</ul>
 
+	<h2>Languages/Locales</h2>
+	<ul>
+		<li>
+			App.defaultLocale: <code><?php echo h(\Cake\Core\Configure::read('App.defaultLocale')); ?></code>
+		</li>
+		<li>
+			App.defaultTimezone: <code><?php echo h(\Cake\Core\Configure::read('App.defaultTimezone')); ?></code>
+		</li>
+		<li>
+			date_default_timezone_get(): <code><?php echo h(date_default_timezone_get()); ?></code>
+		</li>
+		<li>
+			ini_get('intl.default_locale'): <code><?php echo h(ini_get('intl.default_locale')); ?></code>
+		</li>
+		<li>
+
+		</li>
+	</ul>
+
+	<h2>Translations</h2>
+	<?php
+	$translator = \Cake\I18n\I18n::getTranslator();
+	$messages = $translator->getPackage()->getMessages();
+	?>
+	<p><?php echo count($messages);?> translations</p>
+	<details>
+		<summary>Details</summary>
+		<table>
+			<?php foreach ($messages as $message => $details) { ?>
+				<tr>
+					<td><?php echo h($message) ?></td>
+					<td><ul><?php
+						$context = $details['_context'] ?? [];
+						foreach ($context as $key => $translation) {
+							echo '<li>' . ($key ? h($key). ': ' : '') . h($translation). '</li>';
+						}
+						?></ul></td>
+					<?php } ?>
+				</tr>
+		</table>
+	</details>
+
 </section>
