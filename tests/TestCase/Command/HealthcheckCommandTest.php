902c57f50d607b0107c6657a041635b984b80eca
mscherer <dereuromark@web.de>
2025-05-25 23:23:46 +0200

    Enable basic healthcheck.

--- tests/TestCase/Command/HealthcheckCommandTest.php
+++ tests/TestCase/Command/HealthcheckCommandTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Setup\Test\TestCase\Command;
 
 use Cake\Console\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\Core\Configure;
 use Cake\TestSuite\TestCase;
 
 /**
@@ -28,6 +29,8 @@ class HealthcheckCommandTest extends TestCase {
 	 * @return void
 	 */
 	public function testExecute(): void {
+		Configure::write('App.fullBaseUrl', 'https://example.com');
+
 		$this->exec('healthcheck');
 
 		$this->assertOutputContains('=> OK');
